152fb23411005a66beec52d9d59226aa2bb014e0
Aniket <aniketkalawat88@gmail.com>
2025-01-14 16:37:45 +0530

    authentication-add

--- routes/admin.routes.js
+++ routes/admin.routes.js
@@ -4,12 +4,17 @@ const { UpdateAbout, getAbout } = require('../controllers/about.controller');
 
 const router = express.Router();
 
-router.get('/admin' , adminAuth,(req,res) => {
-    return res.status(200).json({message:`Welcome Admin`});
-})
-router.put('/about',UpdateAbout)
+router.put('/about' , adminAuth,UpdateAbout)
 
 router.get('/about',getAbout)
 
 
 module.exports = router;
+
+
+
+
+
+// router.get('/admin' , adminAuth,(req,res) => {
+//     return res.status(200).json({message:`Welcome Admin`});
+// })
--- routes/enquiry.routes.js
+++ routes/enquiry.routes.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const { PostEnquiry, GetEnquiry } = require("../controllers/enquiry.controller");
+const adminAuth = require("../middlewares/auth.middleware");
 
 const router = express.Router();
 
 router.post("/enquiry" , PostEnquiry);
-router.get("/enquiry" , GetEnquiry);
+router.get("/enquiry", adminAuth , GetEnquiry);
 
 module.exports = router
